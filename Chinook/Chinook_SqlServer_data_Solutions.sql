4b6b79122419ce432a795cdb7bd0ee80d3a0c615
Tanishapanwar25 <tanisha.panwar@quantumatix.in>
2025-07-15 14:26:54 +0530

    Add Chinook

---
+++ Chinook/Chinook_SqlServer_data_Solutions.sql
@@ -0,0 +1,12 @@
+select * from Album ;
+select * from Artist where name='Metallica'
+select * from Customer;
+select * from Employee;
+select * from Genre;
+select * from Invoice ;
+select * from InvoiceLine;
+select * from MediaType;
+select * from Playlist;
+select * from PlaylistTrack;
+select * from Track where trackid=254;
+
